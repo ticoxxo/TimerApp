2183e933cd1131f0ed4fa35b393f1021de5b1167
Ticoxxo <ticoxxo@gmail.com>
2024-02-29 08:27:38 -0700

    Initial Commit

---
+++ TimerApp/TimerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerAppApp.swift
+//  TimerApp
+//
+//  Created by Alberto Almeida on 29/02/24.
+//
+
+import SwiftUI
+
+@main
+struct TimerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
