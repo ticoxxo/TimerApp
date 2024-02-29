f851358613f3b77433eef7cbb82396a899cb2cba
Ticoxxo <ticoxxo@gmail.com>
2024-02-29 11:28:24 -0700

    Timer que hace más pequeño su contenedor cada que el counter baja

--- TimerApp/ContentView.swift
+++ TimerApp/ContentView.swift
@@ -7,15 +7,52 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
+    @State private var timeRemaining = 100
+    @State private var widthCustom = CGFloat(Float(350))
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    @Environment(\.scenePhase) var scenePhase
+    @State private var isActive = true
+    @State private var play = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Circle()
+                .stroke(.green, lineWidth: 20)
+                .fill(.white)
+                .frame(maxWidth: widthCustom)
+            HStack {
+                Button("Play", systemImage: !play ? "play.fill" : "stop.fill") {
+                    play.toggle()
+                }
+                    .labelStyle(.iconOnly)
+                Text("\(timeRemaining)")
+                    .onReceive(timer) { time in
+                        guard isActive else { return }
+                        guard play else { return }
+                        
+                        if timeRemaining > 0 {
+                            timeRemaining -= 1
+                            widthCustom -= 2
+                        }
+
+                            
+                    }
+                    .onChange(of: scenePhase) {
+                        if scenePhase == .active {
+                            isActive = true
+                        } else {
+                            isActive = false
+                        }
+                    }
+                Button("Redo", systemImage: "arrow.uturn.forward.circle") {
+                    timeRemaining = 100
+                    widthCustom = 350
+                }
+                
+            }
         }
-        .padding()
     }
 }
 
